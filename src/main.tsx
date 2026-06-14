4cf235799c121bda7a096a7eaf95880229e0f04a
drahmanfitt-cloud <drahmanfitt@gmail.com>
2026-06-14 16:47:36 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import{StrictMode}from'react'
 import{createRoot}from'react-dom/client'
 import'./index.css'
-import app from'./App'
+import App from'./App'
 createRoot(document.getElementById('root')).render(<StrictMode><</StrictMode>)
